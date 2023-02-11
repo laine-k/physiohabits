0face3cd6cb7de6e79dd4076886408ba80824457
laine-k <laine.kaleja@protonmail.com>
2023-02-12 01:18:46 +0200

    remove viteradar

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import ViteRadar from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(), 
-    vueJsx()
+    vueJsx(),
+    ViteRadar({
+      gtm: [
+       {
+         id: 'GTM-54HN2L2',
+       }
+     ]
+})
   ],
   resolve: {
     alias: {
