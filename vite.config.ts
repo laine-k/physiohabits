ec8809a660cb9471197db10f82147ba8c9b81577
laine-k <laine.kaleja@protonmail.com>
2023-02-12 00:34:28 +0200

    gtm

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import ViteRadar from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(), 
+    vueJsx(),
+    ViteRadar({
+           gtm: [
+            {
+              id: 'GTM-54HN2L2',
+            }
+          ],
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
